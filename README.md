04ae13f1544f0eca48ab97dc6de0384411bae66f
dvorav <d.vorav612@gmail.com>
2020-10-17 22:12:44 -0500

    3rd Commit

--- README.md
+++ README.md
@@ -1,1 +1,14 @@
 # 03-Password-Generator
+
+This project is designed to generate a password based on the critieras selected once clicked on "Generate Password". Criterias include, being uppecase, being lowercase, having special characters, or having numbers. Before those critieras, you have the option on how many characters do you want, between 8 and 128 characters.
+
+
+
+![FirstPrompt](image/numberCharacter.PNG)
+![FirstPrompt](image/uppercase.PNG)
+![FirstPrompt](image/lowercase.PNG)
+![FirstPrompt](image/number.PNG)
+![FirstPrompt](image/special.PNG)
+![FirstPrompt](image/result.PNG)
+
+
--- script.js
+++ script.js
@@ -1,140 +1,135 @@
+//A function to generate a password
+function generatePassword() {
+  // Generate lowercase letter
+  let lowerAlp = "abcdefghijklmnopqrstuvwxyz";
+  let arrLower = lowerAlp.split("");
+  //Generate random lowercase
 
-// Generate lowercase letter 
-let lowerAlp = "abcdefghijklmnopqrstuvwxyz";
-let arrLower = lowerAlp.split('');
-//Generate random lowercase
-let randomLower = arrLower[Math.floor(Math.random()*26)];
+  // Generate uppercase letter
+  arrUpper = arrLower.map(function (x) {
+    return x.toUpperCase();
+  });
 
-// Generate uppercase letter
-arrUpper = arrLower.map(function(x){return x.toUpperCase();})
-//Generate random uppercase
-let randomUpper = arrLower[Math.floor(Math.random()*26)];
+  //Generate number
+  let num = "1234567890";
+  let arrNum = num.split("");
 
-//Generate number 
-let num = "1234567890";
-let arrNum = num.split('');
-//Generate random number
-let randomNumber = arrNum[Math.floor(Math.random()*arrNum.length)];
+  //Generate special character
+  let specChar = "!\"#$%&'()*+,-./:;<=>?@[]^_`{|}~";
+  let arrSpec = specChar.split("");
 
-//Generate special character 
-let specChar =  "!\"#$%&'()*+,-./:;<=>?@[\]^_`{|}~"
-let arrSpec = specChar.split('');
-//Generate random symbol
-let randomSpecial = arrSpec[Math.floor(Math.random()*arrSpec.length)];
- 
+  //User Input for Password Length
+  const userLength = parseInt(prompt("Enter a number between 8 and 128, for how many characters you'd like."));
+  let hasLower = "";
+  let hasUpper = "";
+  let hasNum = "";
+  let hasSpec = "";
 
+  //userLength has no input
+  if (!userLength) {
+    alert("Invalid Input");
 
+    //userLength is not a number between 8 and 128
+  } else if (userLength < 8 || userLength > 128) {
+    alert("Has to be a number between 8 and 128");
+    password = "  ";
+  }
+  //userLength meets criteria
+  else {
+    hasLower = confirm("Is there lowercases?");
+    hasUpper = confirm("Is there uppercases?");
+    hasNum = confirm("Is there numbers?");
+    hasSpec = confirm("Is there special characters?");
+  }
 
-//User Input for Password Length
-const userLength = parseInt(prompt("Enter a number between 8 and 128"));
-let hasLower = '';
-let hasUpper = '';
-let hasNum = '';
-let hasSpec = ''; 
+  //Different combinations based on criterias
+  //Randomize arrays
+  let randomUpper = arrUpper[Math.floor(Math.random() * 26)];
+  let randomLower = arrLower[Math.floor(Math.random() * 26)];
+  let randomNumber = arrNum[Math.floor(Math.random() * arrNum.length)];
+  let randomSpecial = arrSpec[Math.floor(Math.random() * arrSpec.length)];
 
-//userLength has no input                               
- if (!userLength) {
-   alert("Invalid Input")
- //userLength is not a number between 8 and 128  
- } else if (userLength < 8 || userLength > 128) {
-   alert("Has to be a number between 8 and 128")
- }
-//userLength meets criteria
- else {
-   hasLower = confirm("Is there lowercases?");
-   hasUpper = confirm("Is there uppercases?");
-   hasNum = confirm("Is there numbers?");
-   hasSpec = confirm("Is there special characters?");  
- }
+  //All 4 criterias = false
+  if (!hasLower && !hasUpper && !hasNum && !hasSpec) {
+    password.value = "Need to select a option!";
+  }
+  //All 4 criterias = true
+  else if (hasLower && hasUpper && hasNum && hasSpec) {
+    password = randomUpper + randomLower + randomNumber + randomSpecial;
+  }
 
-//Different combinations based on criterias
+  //3 Criterias = true
+  //Uppercase, special, lowercase
+  else if (hasUpper && hasSpec && hasLower) {
+    password = randomUpper + randomSpecial + randomLower;
+  }
+  //Uppercase, special, number
+  else if (hasUpper && hasSpec && hasNum) {
+    password = randomUpper + randomSpecial + randomNumber;
+  }
+  //Uppercase, lowercase, number
+  else if (hasUpper && hasLower && hasNum) {
+    password = randomUpper + randomLower + randomNumber;
+  }
+  //Lowercase, number, special
+  else if (hasLower && hasNum && hasSpec) {
+    password = randomLower + randomNumber + randomSpecial;
+  }
 
-//All 4 criterias = false
-if (!hasLower && !hasUpper && !hasNum && !hasSpec) {
-  password.value = "Need to select a option!"
+  //2 Criterias = true
+  //Uppercase and lowercase
+  else if (hasUpper && hasLower) {
+    password = randomUpper + randomLower;
+  }
+  //Uppercase and special
+  else if (hasUpper && hasSpec) {
+    password = randomUpper + randomSpecial;
+  }
+  //Uppercase and number
+  else if (hasUpper && hasNum) {
+    password = randomUpper + randomNumber;
+  }
+  //Number and special
+  else if (hasNum && hasSpec) {
+    password = randomNumber + randomSpecial;
+  }
+  //Special and Number
+  else if (hasSpec && hasUpper) {
+    password = randomSpecial + randomUpper;
+  }
+  //Lowercase and number
+  else if (hasLower && hasNum) {
+    password = randomLower + randomNumber;
+  }
+
+  //1 criteria = true
+  //Uppercase = true
+  else if (hasUpper) {
+    password = randomUpper;
+  }
+  //Lowercase = true
+  else if (hasLower) {
+    password = randomLower;
+  }
+  //Number = true
+  else if (hasNum) {
+    password = randomNumber;
+  }
+  //Special = true
+  else if (hasSpec) {
+    password = randomSpecial;
+  }
+
+  let generatedps = "";
+  //Generate Password
+  for (i = 0; i < userLength; i++) {
+    generatedps += password[Math.floor(Math.random() * password.length)];
+    if (generatedps > 8) {
+    }
+  }
+  document.querySelector("#password").value = generatedps;
 }
-//All 4 criterias = true
-else if (hasLower && hasUpper && hasNum && hasSpec) {
-  password = arrLower.concat(arrNum, arrSpec, arrUpper)
-}
-// testing 
 
-//3 Criterias = true 
-else if (hasLower && hasUpper && hasNum ) {
-  password = "3 Criterias = Functional"
-}
-else if (hasLower && hasUpper && hasSpec) {
-  password = "3 Criterias = functional"
-}
-else if (hasLower && hasNum && hasSpec) {
-  password = "3 Criterias = functional"
-}
-else if (hasNum && hasUpper && hasSpec) {
-  password = "3 Criterias = functional"
-}
-
-//2 Criterias = true 
-else if (hasUpper && hasLower) {
-  password = "2 Criterias = Functional"
-}
-else if (hasUpper && hasNum) {
-  password = "2 Criterias = Functional"
-}
-else if (hasUpper && hasSpec) {
-  password = "2 Criterias = Functional"
-}
-else if (hasLower && hasSpec) {
-  password = "2 Criterias = Functional"
-}
-else if (hasLower && hasNum) {
-  password = "2 Criterias = Functional"
-}
-else if (hasSpec && hasNum) {
-  password = "2 Criterias = Functional"
-}
-
-//1 criteria = true 
-else if (hasSpec) {
-  password = "1 Criterias = Functional"
-}
-else if (hasNum) {
-  password = "1 Criterias = Functional"
-}
-else if (hasLower) {
-  password = "1 Criterias = Functional"
-}
-else if (hasUpper) {
-  password = "1 Criterias = Functional"
-}
-
-
-/* Trying to figure out formula to generate password
-for (i = 0; i < userLength; i++) {
-  var pickChoices = password[Math.floor(Math.random() * password.length)];
-  password.push(pickChoices); 
-} 
-*/
-
-
-/* Implented code into HW 
-// Assignment Code
+//Generate password to HTML textarea
 let generateBtn = document.querySelector("#generate");
-// Write password to the #password input
-function writePassword() { 
- // let password = generatePassword();
-  let passwordText = document.querySelector("#password");
-  passwordText.innerHTML = "Hello Layla";
-};
-// Add event listener to generate button
-generateBtn.addEventListener("click", writePassword() {
-}
-*/
-
-
-//Generate password to textarea
-let generateBtn = document.querySelector("#generate");
-let passToHtml = function() {
-  document.querySelector("#password").innerHTML = password /* Where to place item id */
-}
-
-generateBtn.addEventListener("click", passToHtml)
+generateBtn.addEventListener("click", generatePassword);
